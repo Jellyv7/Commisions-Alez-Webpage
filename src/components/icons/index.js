c91cf4e8c1383bb7180f255fc591b282752435d4
Jelly.flac <pinkshade1709@gmail.com>
2024-04-26 18:36:52 -0400

    first commit

---
+++ src/components/icons/index.js
@@ -0,0 +1,11 @@
+
+export {default as Home} from './Home.astro';
+export {default as Commisions} from './Commisions.astro';
+export {default as Portfolio} from './Portfolio.astro';
+export {default as Terms} from './Terms.astro';
+export {default as Instagram} from './Instagram.astro'
+export {default as Discord} from './Discord.astro';
+export {default as Twitter} from './Twitter.astro';
+export {default as YouTube} from './YouTube.astro'
+export {default as Kofi} from './Kofi.astro';
+
