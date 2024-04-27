c7949b6125f05bfe55b0574b60b79e257e846ed8
Jelly.flac <pinkshade1709@gmail.com>
2024-04-27 15:52:04 -0400

    transmform image to webp

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
-export {default as example1} from './examplecom1.jpg';
-export {default as example2} from './examplecom2.jpg';
-export {default as example3} from './examplecom3.jpg';
-export {default as example4} from './examplecom4.jpg';
-export {default as example5} from './examplecom5.jpg';
-export {default as example6} from './examplecom6.jpg';
+export {default as example1} from './examplecom1.webp';
+export {default as example2} from './examplecom2.webp';
+export {default as example3} from './examplecom3.webp';
+export {default as example4} from './examplecom4.webp';
+export {default as example5} from './examplecom5.webp';
+export {default as example6} from './examplecom6.webp';
