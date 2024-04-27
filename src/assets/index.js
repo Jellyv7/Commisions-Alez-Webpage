564a0df0f509b236306d0dee6a3a87554852b611
Jelly.flac <pinkshade1709@gmail.com>
2024-04-27 15:30:40 -0400

    create commisions page

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export {default as example1} from './examplecom1.jpg';
+export {default as example2} from './examplecom2.jpg';
+export {default as example3} from './examplecom3.jpg';
+export {default as example4} from './examplecom4.jpg';
+export {default as example5} from './examplecom5.jpg';
+export {default as example6} from './examplecom6.jpg';
