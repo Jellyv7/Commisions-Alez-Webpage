dbb8fa8222df18e2b76da0308263854be30c4ef2
Jelly.flac <pinkshade1709@gmail.com>
2024-05-16 17:42:08 -0400

    fix css and media queries

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,9 @@
-export {default as example1} from './examplecom1.webp';
-export {default as example2} from './examplecom2.webp';
-export {default as example3} from './examplecom3.webp';
-export {default as example4} from './examplecom4.webp';
-export {default as example5} from './examplecom5.webp';
-export {default as example6} from './examplecom6.webp';
+export {default as example1} from './hyeseoredraw.jpg';
+export {default as example2} from './mikuuuu2.jpg';
+export {default as example3} from './photo-couple-new.jpg';
+export {default as example4} from './girl-w-n-glasses.jpg';
+export {default as example5} from './farcille-pre.png';
+export {default as example6} from './madohomu.png';
 export {default as portfolio1} from './portfolio1.jpg';
 export {default as portfolio2} from './portfolio2.jpg';
 export {default as portfolio3} from './portfolio3.jpg';
