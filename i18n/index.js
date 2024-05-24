242ffda45ce60c9ec70b40ac4300c3565c961ad7
Jelly.flac <pinkshade1709@gmail.com>
2024-05-24 17:16:03 -0400

    fix minor bugs

--- i18n/index.js
+++ i18n/index.js
@@ -1,13 +1,8 @@
 import { ENGLISH } from './en.json'
 import { SPANISH } from './es.json';
 
-const LANGUAGES = {
-	SPANISH: 'es',
-	ENGLISH: 'en'
-}
-
 export const getI18N = (currentLocale = 'es') => {
-	if (currentLocale === LANGUAGES.SPANISH) return SPANISH;
-	if (currentLocale === LANGUAGES.ENGLISH) return ENGLISH;
+	if (currentLocale === 'es') return SPANISH;
+	if (currentLocale === 'en') return ENGLISH;
 	return ENGLISH;
 }
